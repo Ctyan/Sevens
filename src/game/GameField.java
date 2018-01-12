391fb5a83d1fc37a6cf6ada571c435961e46adec
Ctyan <im12800a@gmail.com>
2018-01-12 22:05:40 +0900

    GameFieldの変更

--- src/game/GameField.java
+++ src/game/GameField.java
@@ -14,9 +14,54 @@ public class GameField {
 		diaLine = new boolean[13];
 	}
 
-	public void setCard(int t, int n) {
+	public boolean setCard(int t, int n) {
+		if(isCard(t, n)) return false;
 
+		switch (t) {
+		case 1:
+			spadeLine[n] = true;
+			break;
+
+		case 2:
+			heartLine[n] = true;
+			break;
+
+		case 3:
+			clubLine[n] = true;
+			break;
+
+		case 4:
+			diaLine[n] = true;
+			break;
+
+		default:
+			break;
+		}
+		return true;
 	}
 
-	// public boolean
+	public boolean isCard(int t, int n) {
+		boolean b = false;;
+		switch (t) {
+		case 1:
+			b = spadeLine[n];
+			break;
+
+		case 2:
+			b = heartLine[n];
+			break;
+
+		case 3:
+			b = clubLine[n];
+			break;
+
+		case 4:
+			b = diaLine[n];
+			break;
+
+		default:
+			break;
+		}
+		return b;
+	}
 }
