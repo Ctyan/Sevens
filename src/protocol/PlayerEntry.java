4def50b5e3af794cd2ed3687a798b13b7747edba
trione <godgoodgodwin1996@gmail.com>
2018-01-13 17:50:42 +0900

    参加できたか, 一番最初の参加者かの真偽値を追加

--- src/protocol/PlayerEntry.java
+++ src/protocol/PlayerEntry.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 public class PlayerEntry implements Serializable{
 	private static final long serialVersionUID = 764086852042470980L;
 	private String player_name;
+	private boolean isEntry;
+	private boolean isFirst;
 	
 	public PlayerEntry(String player_name) {
 		this.player_name = player_name;
@@ -22,5 +24,22 @@ public class PlayerEntry implements Serializable{
 	public void setPlayer_name(String player_name) {
 		this.player_name = player_name;
 	}
+
+	public boolean isEntry() {
+		return isEntry;
+	}
+
+	public void setEntry(boolean isEntry) {
+		this.isEntry = isEntry;
+	}
+
+	public boolean isFirst() {
+		return isFirst;
+	}
+
+	public void setFirst(boolean isFirst) {
+		this.isFirst = isFirst;
+	}
+	
 	
 }
