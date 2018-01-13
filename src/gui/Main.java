0ae1103bb619b54ee264026e12246fdef4d91a12
trione <godgoodgodwin1996@gmail.com>
2018-01-13 10:52:46 +0900

    Main競合解消

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-package gui;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-
-public class Main extends Application {
-	public static GUIManager manager;
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			/*
-			 * Start
-			 * RuluSettings
-			 * Wait
-			 * Play
-			 * Ranking
-			 */
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("Play.fxml"));
-			Parent root = loader.load();
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//画面遷移に必要
-			manager = new GUIManager();
-			manager.setThisStage(primaryStage);
-			//表示
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
-}
-=======
 package gui;
 
 import javafx.application.Application;
@@ -80,4 +38,3 @@ public class Main extends Application {
 		launch(args);
 	}
 }
->>>>>>> refs/remotes/CtyanRemoteSevensMaster/gui_system
