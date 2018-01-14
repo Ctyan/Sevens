d0e65ea5f6de281b7f925a113afdb3029196ff08
trione <godgoodgodwin1996@gmail.com>
2018-01-14 21:41:27 +0900

    ゲーム開始用プロトコル:GameStarterKitの追加と対応

---
+++ src/protocol/GameStarterKitProtocol.java
@@ -0,0 +1,9 @@
+package protocol;
+
+public class GameStarterKitProtocol extends Protocol{
+	public GameStarterKitProtocol(GameStarterKit starterKit) {
+		super(Protocol.GAME_STARTER_KIT);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
