bde17378cf89d0d5e9c9dc2b005c4c9272a9c165
trione <godgoodgodwin1996@gmail.com>
2018-01-14 23:59:02 +0900

    ルール設定者のIDとNameをフィールドに追加

--- src/protocol/GameRule.java
+++ src/protocol/GameRule.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 
 public class GameRule implements Serializable{
 	private static final long serialVersionUID = 717700048603385261L;
+	private int settingPlayerID;
+	private String settingPlayerName;
 	private int round;
 	private int pass;
 	private boolean joker;
 	private boolean tunnel;
 	
-	public GameRule(int round, int pass, boolean joker, boolean tunnel) {
+	public GameRule(int settingPlayerID, String settingPlayerName ,int round, int pass, boolean joker, boolean tunnel) {
+		this.settingPlayerID = settingPlayerID;
+		this.settingPlayerName = settingPlayerName;
 		this.round = round;
 		this.pass = pass;
 		this.joker = joker;
@@ -50,5 +54,21 @@ public class GameRule implements Serializable{
 	
 	public String toString() {
 		return "Round="+this.round+", Pass="+this.pass+", Joker="+this.joker+", Tunnel="+this.tunnel;
+	}
+
+	public int getSettingPlayerID() {
+		return settingPlayerID;
+	}
+
+	public void setSettingPlayerID(int settingPlayerID) {
+		this.settingPlayerID = settingPlayerID;
+	}
+
+	public String getSettingPlayerName() {
+		return settingPlayerName;
+	}
+
+	public void setSettingPlayerName(String settingPlayerName) {
+		this.settingPlayerName = settingPlayerName;
 	}
 }
