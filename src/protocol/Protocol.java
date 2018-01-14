941dfd3f9972452006c45eb25d6ccfb7c7504db9
trione <godgoodgodwin1996@gmail.com>
2018-01-14 13:31:42 +0900

    Protocol_Boolのアクセス修飾子をprotectedへ変更

--- src/protocol/Protocol.java
+++ src/protocol/Protocol.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Protocol implements Serializable{
 	private static final long serialVersionUID = 1861050339906354967L;
-	private boolean Protocol_Bool;
+	protected boolean Protocol_Bool;
 	private Integer Protocol_ID;
 	public static int CHAT = 0,
 					  GAME = 1,
